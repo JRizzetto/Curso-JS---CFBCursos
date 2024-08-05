9f4a1c6c5c77857487745f12ab2a1b1a3438c9c1
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-08-05 15:58:20 -0300

    Diferença entre Pré Incremento e Pós Incremento

---
+++ aula09.js
@@ -0,0 +1,14 @@
+
+// let n=10;
+
+// n++; // Pós incremento - Estou incrementendo em um, ou seja, n=11; Eu uso o valor e depois eu incremento. 
+
+// console.log(n++); // Nesse caso o resultado será 10, pois ele primeiro pega o resultado e só depois incrementa.
+
+// console.log(n++); // Pré incremento - Dessa forma primeiro o valor é incrementado e só depois ele imprime o valor. 
+
+let n1=10;
+let n2=20;
+
+console.log(n1 + "" + n2); // Sabesmos que o sinal + ele é para soma, porém, a qualquer lugar, ele considera que o + vira um operador de contatenação. 
+
