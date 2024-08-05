fa83ebf69b3d3c38b6635a06d3586ec5af9e2730
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-08-05 15:33:57 -0300

    Operadores Bitwise

---
+++ aula08.js
@@ -0,0 +1,20 @@
+/*
+let n1=13;
+let n2=14;
+
+// let res = n1 & n2;
+
+// let res = n1 | n2;
+
+let res = n1 ^ n2;
+
+console.log(res);
+*/
+
+let n1 = 30;
+
+// let res = n1 << 1
+
+let res = n1 >> 1
+
+console.log(res); 
