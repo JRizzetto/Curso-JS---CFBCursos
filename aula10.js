04296854cba7c42ac8e4c7adf89bb922b0448239
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-08-06 09:34:25 -0300

    Operador Ternário

---
+++ aula10.js
@@ -0,0 +1,30 @@
+/*
+let num=11;
+
+let res=num%2;
+
+ if(res==0){
+    console.log("Par");
+}else {
+    console.log("impar")
+}
+    */ // Nessa operação temos uma operação em que o resto sempre vai ser 0 ou 1, assim, fizemos um sistema com if para imprimir sempre par ou impar. 
+
+
+    /*
+    let num=10;
+    res= !(num%2) ? "Par" : "Impar";
+    console.log(res);
+    */
+
+    /*    
+    let num1 = 2;
+    let num2 = 5;
+    res=(num1 > num2 ? "Verdadeiro" : "Falso")
+    console.log(res);
+    */
+
+    let st="A"
+
+    res=(st == "A" ? "Ativo" : "Inativo");
+    console.log(res)
