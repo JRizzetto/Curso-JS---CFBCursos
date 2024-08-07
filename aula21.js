6a6a3b521b9826898ba8d71b832a1750e53bebdf
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-08-07 11:55:12 -0300

    FUNÇÕES com retorno

---
+++ aula21.js
@@ -0,0 +1,15 @@
+
+function canal(){
+    let n1=7;
+    let n2=3;
+    let res=n1*n2;
+
+    if(res%2==0){
+        return "Par"
+    }else{
+        return "Impar"
+    }
+}
+
+let result=canal();
+console.log(result);
