0054db0427d611fb6da4028249ed397abf0f75f9
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-08-07 12:24:08 -0300

    FUNÇÕES Parametrizadas

---
+++ aula22.js
@@ -0,0 +1,46 @@
+
+
+
+/*
+function soma(p1){
+    console.log(p1)
+}
+
+soma("21");
+soma("Jefferson");
+soma(33 );
+*/
+
+/*
+function soma(n1=5, n2=10){
+    console.log(n1+n2);
+}
+
+soma();
+*/
+
+/*
+function soma(n1=0, n2=0){
+    let res;
+    res = n1+n2;
+    return res;
+}
+
+let resSoma=soma(5,8);
+console.log(resSoma);
+
+console.log(soma(10+7));
+*/
+
+let valor=0
+console.log(valor);
+
+function add(v){
+    return valor + v;
+}
+
+valor=add(10);
+console.log(valor);
+
+valor=add(5);
+console.log(valor);
