655c6ed5435008f005fde08af4fecf190882e03a
JRizzetto <104041862+JRizzetto@users.noreply.github.com>
2024-08-09 15:52:36 -0300

    Relação dos elementos no DOM 2

---
+++ aula39.js
@@ -0,0 +1,32 @@
+const caixa1=document.querySelector("#caixa1");
+const btnEl=[...document.querySelectorAll(".curso")];
+const c1_2=document.querySelector("#c1_2");
+
+// console.log(c1_2.parentNode);
+console.log(c1_2.parentNode.parentNode.parentNode.children[4]);
+
+// console.log(caixa1.lastElementChild)
+// console.log(caixa1.firstElementChild)
+// console.log(caixa1.children)
+// console.log(document.getRootNode())
+// console.log(btnEl[0].getRootNode());
+// console.log(btnEl[0].ownerDocument);
+
+// console.log(caixa1.hasChildNodes())
+// console.log(btnEl[0].hasChildNodes())
+// console.log(btnEl[0].childNodes)
+// // console.log(btnEl[0].children)
+
+// if (caixa1.children.length > 0){
+//     console.log("Esse elemento possui filhos")
+// }else{
+//     console.log("Não possui filhos")
+// }
+
+// console.log(caixa1.children.length > 0 ? "Possui Filhos" : "Não possui filhos")
+// console.log(btnEl[0].children.length > 0 ? "Possui Filhos" : "Não possui filhos")
+
+// console.log(caixa1.firstElementChild.innerHTML="Teste");
+
+// console.log(caixa1.children[1].innerHTML="teste 2");
+
